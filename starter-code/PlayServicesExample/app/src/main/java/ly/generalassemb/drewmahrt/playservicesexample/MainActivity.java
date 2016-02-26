66c38e6ea812689dc341606fd47f075258b4f837
roberrera <roberterrera@gmail.com>
2016-02-25 23:32:36 -0500

    Add permissions check, create layout

--- starter-code/PlayServicesExample/app/src/main/java/ly/generalassemb/drewmahrt/playservicesexample/MainActivity.java
+++ starter-code/PlayServicesExample/app/src/main/java/ly/generalassemb/drewmahrt/playservicesexample/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,27 +22,123 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+import java.util.Map;
 
-    Tracker mTracker;
-    GoogleApiClient mGoogleApiClient;
-    Location mLastLocation;
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COURSE_LOCATION = 1;
+    private Tracker mTracker;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private TextView mLatitudeText, mLongitudeText;
+    private  Button mButton;
+    private  MapView mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mButton = (Button) findViewById(R.id.button_map);
+        mMap = (MapView) findViewById(R.id.mapview);
+        mLatitudeText = (TextView) findViewById(R.id.textview_latitude);
+        mLongitudeText = (TextView) findViewById(R.id.textview_longitude);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* AppCompatActivity */,
-                        this /* OnConnectionFailedListener */)
-                .addApi()
+                .enableAutoManage(this, this)
+                .addApi(LocationServices.API)
                 .build();
+
 
         AnalyticsApplication application = (AnalyticsApplication) getApplication();
         mTracker = application.getDefaultTracker();
 
+        // TODO: Fix connectionHint error.
+
+        @Override
+        public void onConnected (Bundle connectionHint){
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                        mGoogleApiClient);
+
+                RequestPermissionsAsyncTask requestPermissionsAsyncTask = new RequestPermissionsAsyncTask();
+                requestPermissionsAsyncTask.execute();
+
+                return;
+            }
+
+        }
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mLastLocation != null) {
+                    mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+                    mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+                    // TODO: Add map intent
+                }
+
+            }
+        });
+
+    }
+
+    public class RequestPermissionsAsyncTask extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_COURSE_LOCATION);
+
+            return null;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COURSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
@@ -50,10 +148,30 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         mTracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
 
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         // An unresolvable error has occurred and a connection to Google APIs
         // could not be established. Display an error message, or handle
         // the failure silently
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 }
